82489902e185a16c277dd5379ee00997b7b2538d
usung <1seconds@naver.com>
2022-06-30 11:29:01 +0900

    [클라/수정] 샘플데이터 추가 1. resourceStatic/UI/Common 공통이미지 업데이트 완료 2. Resources/Prefabs/InGame/G100_GameName/Sample1,2확인 Sample1,2를 Canvas/DefaultUI/InGame오브젝트로 대체해서 사용 3. Canvas/Popup/ClearBottom추가함 - 메소드중에 public으로 되어잇는거 테스트해보고 적절한거 가져다가 사용하시면됩니다. 4. Resources/Anims/InGame/ox 스파인 추가함

---
+++ Assets/Scripts/Application/Common/UI/BottomPanel.cs
@@ -0,0 +1,142 @@
+﻿/* 
+* Copyright (C) Decartes Corp. All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* 
+* @author You sungHyun <1seconds@biscuitlabs.io>
+* @created 2022/06/30
+* @desc BottomPanel Template
+*/
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public delegate void OnBottomPanelEvent(float height);
+
+public class BottomPanel : ResultPopup {
+    [SerializeField] private RectTransform anchor;
+    [SerializeField] private RectTransform wrongAnchor2;
+    [SerializeField] private float height = 100;
+
+    public bool isShown { get; private set; }
+    public event OnBottomPanelEvent onMoving;
+
+    private void Awake() {
+        HideImmediately();
+        if (wrongAnchor2 != null) {
+            HideImmediately2();
+        }
+    }
+
+    private void HideImmediately() {
+        var to = new Vector3(0, -height - 150, 0);
+        anchor.anchoredPosition = to;
+        anchor.gameObject.SetActive(false);
+        isShown = false;
+    }
+    
+    private void HideImmediately2() {
+        var to = new Vector3(0, -height - 150, 0);
+        wrongAnchor2.anchoredPosition = to;
+        wrongAnchor2.gameObject.SetActive(false);
+        isShown = false;
+    }
+
+    public void Show() {
+        anchor.gameObject.SetActive(true);
+        isShown = true;
+        StartCoroutine(ShowRoutine());
+    }
+    
+    public void Show2() {
+        wrongAnchor2.gameObject.SetActive(true);
+        isShown = true;
+        StartCoroutine(ShowRoutine2());
+    }
+
+    public void Hide(bool easing) {
+        isShown = false;
+        if (easing == false || !this.gameObject.activeInHierarchy) {
+            HideImmediately();
+        } else {
+            StartCoroutine(HideRoutine());
+        }
+    }
+    
+    public void Hide2(bool easing) {
+        isShown = false;
+        if (easing == false || !this.gameObject.activeInHierarchy) {
+            HideImmediately2();
+        } else {
+            StartCoroutine(HideRoutine2());
+        }
+    }
+
+    private IEnumerator ShowRoutine() {
+        var to = Vector3.zero;
+        Vector3 from = anchor.anchoredPosition;
+        var easeType = EaseType.easeOutQuad;
+        var duration = 0.3f;
+
+        var ease = new EaseRunner(easeType, duration);
+        while (ease.IsPlaying()) {
+            var progress = ease.Run();
+            var pt = Vector3.Lerp(from, to, progress);
+            anchor.anchoredPosition = pt;
+            onMoving?.Invoke(height + pt.y);
+            yield return null;
+        }
+    }
+    
+    private IEnumerator ShowRoutine2() {
+        var to = Vector3.zero;
+        Vector3 from = wrongAnchor2.anchoredPosition;
+        var easeType = EaseType.easeOutQuad;
+        var duration = 0.3f;
+
+        var ease = new EaseRunner(easeType, duration);
+        while (ease.IsPlaying()) {
+            var progress = ease.Run();
+            var pt = Vector3.Lerp(from, to, progress);
+            wrongAnchor2.anchoredPosition = pt;
+            onMoving?.Invoke(height + pt.y);
+            yield return null;
+        }
+    }
+
+    private IEnumerator HideRoutine() {
+        var to = new Vector3(0, -height - 150, 0);
+        Vector3 from = anchor.anchoredPosition;
+        var easeType = EaseType.easeOutQuad;
+        var duration = 0.3f;
+
+        var ease = new EaseRunner(easeType, duration);
+        while (ease.IsPlaying()) {
+            var progress = ease.Run();
+            var pt = Vector3.Lerp(from, to, progress);
+            anchor.anchoredPosition = pt;
+            onMoving?.Invoke(height + pt.y);
+            yield return null;
+        }
+    
+        anchor.gameObject.SetActive(false);
+    }
+    
+    private IEnumerator HideRoutine2() {
+        var to = new Vector3(0, -height - 150, 0);
+        Vector3 from = wrongAnchor2.anchoredPosition;
+        var easeType = EaseType.easeOutQuad;
+        var duration = 0.3f;
+
+        var ease = new EaseRunner(easeType, duration);
+        while (ease.IsPlaying()) {
+            var progress = ease.Run();
+            var pt = Vector3.Lerp(from, to, progress);
+            wrongAnchor2.anchoredPosition = pt;
+            onMoving?.Invoke(height + pt.y);
+            yield return null;
+        }
+    
+        wrongAnchor2.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Application/Common/UI/ResultPopup.cs
@@ -0,0 +1,129 @@
+﻿/* 
+* Copyright (C) Decartes Corp. All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* 
+* @author You sungHyun <1seconds@biscuitlabs.io>
+* @created 2022/06/30
+* @desc ResultPopup Template
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Spine.Unity;
+
+public class ResultPopup : MonoBehaviour {
+    [SerializeField] private SkeletonGraphic graphic;
+    [SerializeField] private Slider progressSlider;
+    
+    private Coroutine correctRoutine;
+    private Coroutine wrongRoutine;
+/*
+    public void ResultRoutine(PlayLog log) {
+        if (log.category != GameCategory.PlusSleep) {
+            SoundManager.instance.PlaySFX("clear");
+        }
+        
+        if (progressSlider != null) {
+            StartCoroutine(ProgressSliderEasingRoutine((log.goal - 1),log.goal, log.goal));
+        }
+
+        log.progress += 1;
+        Service.trainer.SaveCountData(log.category);
+        Service.trainer.SaveToday();
+        Service.trainer.SaveEData(log.category, 10);
+        var data = Service.trainer.LoadSLData(log.category);
+        Service.trainer.SaveSLData(log.category, data);
+        Service.trainer.SaveSLToday(log.category, Service.trainer.GetDifficulty(log.category));
+        GameSceneController.stopDoTime = true;
+    }
+
+    public void ProgressRoutine(PlayLog log) {
+        if (log.category != GameCategory.PlusSleep) {
+            SoundManager.instance.PlaySFX("clear2");
+        }
+        
+        if (progressSlider != null) {
+            StartCoroutine(ProgressSliderEasingRoutine((log.progress % log.goal),((log.progress + 1) % log.goal), log.goal));
+        }
+        
+        log.progress += 1;
+        Service.trainer.SaveToday();
+    }
+*/
+    IEnumerator ProgressSliderEasingRoutine(int from, int to, int max) {
+        progressSlider.maxValue = max;
+        yield return this.Ease(EaseType.linear, 0.3f, (v) => {
+            progressSlider.value =  Mathf.Lerp(from, to, v);
+        });
+    }
+
+    public void OnCorrect() {
+        graphic.gameObject.SetActive(true);
+        graphic.Skeleton.SetSkin("skin1");
+        graphic.Skeleton.SetSlotsToSetupPose();
+        graphic.LateUpdate();
+        
+        graphic.AnimationState.SetAnimation(0, "start", false);
+        if (correctRoutine != null) {
+            StopCoroutine(correctRoutine);
+        }
+        correctRoutine = StartCoroutine(OnCorrectRoutine());
+    }
+    /*
+    public void OnCorrect(GameCategory category) {
+        graphic.gameObject.SetActive(true);
+        graphic.Skeleton.SetSkin("skin1");
+        graphic.Skeleton.SetSlotsToSetupPose();
+        graphic.LateUpdate();
+        
+        graphic.AnimationState.SetAnimation(0, "start", false);
+        if (correctRoutine != null) {
+            StopCoroutine(correctRoutine);
+        }
+        correctRoutine = StartCoroutine(OnCorrectRoutine());
+        //Service.trainer.SaveStageIndex(category, Service.trainer.GetDifficulty(category));
+    }*/
+
+    public void OnWrong() {
+        graphic.gameObject.SetActive(true);
+        graphic.Skeleton.SetSkin("skin2");
+        graphic.Skeleton.SetSlotsToSetupPose();
+        graphic.LateUpdate();
+        
+        graphic.AnimationState.SetAnimation(0, "start", false);
+        //if (Service.setting.value.isVibrateOn) {
+        //    Handheld.Vibrate();
+        //}
+        //SoundManager.instance.PlaySFX("wrong");
+        if (wrongRoutine != null) {
+            StopCoroutine(wrongRoutine);
+        }
+        wrongRoutine = StartCoroutine(OnWrongRoutine());
+    }
+    
+    IEnumerator OnCorrectRoutine() {
+        yield return new WaitForSeconds(1.5f);
+        graphic.gameObject.SetActive(false);
+    }
+
+    IEnumerator OnWrongRoutine() {
+        yield return new WaitForSeconds(1.5f);
+        graphic.gameObject.SetActive(false);
+    }
+
+    public void OnWrongAndWait() {
+        graphic.gameObject.SetActive(true);
+        graphic.Skeleton.SetSkin("skin2");
+        graphic.Skeleton.SetSlotsToSetupPose();
+        graphic.LateUpdate();
+
+        graphic.AnimationState.SetAnimation(0, "start", false);
+        if (wrongRoutine != null) {
+            StopCoroutine(wrongRoutine);
+        }
+        wrongRoutine = StartCoroutine(OnWrongRoutine());
+    }
+}
